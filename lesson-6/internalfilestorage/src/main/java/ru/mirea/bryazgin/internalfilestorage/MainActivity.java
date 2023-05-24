b6e066767fbe7597bbeddaba2b44ceffffd8daa6
EgorBR <e.bryazgin@yandex.ru>
2023-05-24 23:57:20 +0300

    lesson 6

---
+++ lesson-6/internalfilestorage/src/main/java/ru/mirea/bryazgin/internalfilestorage/MainActivity.java
@@ -0,0 +1,78 @@
+package ru.mirea.bryazgin.internalfilestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import ru.mirea.bryazgin.internalfilestorage.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String fileName = "history.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                binding.editHistory.post(new Runnable() {
+                    public void run() {
+                        binding.editHistory.setText(getTextFromFile());
+                    }
+                });
+            }
+        }).start();
+    }
+
+    public void buttonOnClick(View view){
+        String string = String.valueOf(binding.editHistory.getText());
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTextFromFile() {
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(fileName);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            Log.d(LOG_TAG, text);
+            return text;
+        } catch (IOException ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return null;
+    }
+}
