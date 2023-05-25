41d965f0e8fc84f14dcde875002b6270b0083b63
EgorBR <e.bryazgin@yandex.ru>
2023-05-26 00:18:51 +0300

    lesson 7

---
+++ lesson-7/app/src/main/java/ru/mirea/bryazgin/lesson7/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.bryazgin.lesson7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
